b119e602f7c5be4abb8d70910fc23cfbdbba071b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-29 20:45:23 +0200

    davinci: build vendor.qti.hardware.camera.device
    Change-Id: I736e19ff5ded1c493d61f6ed400c6decab318c8c

--- device.mk
+++ device.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
