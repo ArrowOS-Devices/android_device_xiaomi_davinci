8afd14cb7d987de789026e95d3639280f0a170cd
RemyL <remy.lampure@free.fr>
2023-05-06 14:19:32 +0200

    davinci: ArrowOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/arrow_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    arrow_davinci-user \
+    arrow_davinci-userdebug \
+    arrow_davinci-eng
--- device.mk
+++ device.mk
@@ -87,7 +87,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # Power
 PRODUCT_COPY_FILES += \
